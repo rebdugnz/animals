a21fc69915280264c348ff962f81215e0cb3c6ca
Tay Hammond <talorjhammond@gmail.com>
2018-06-22 14:59:40 +1200

    added sound functionality and a tiger sound

--- client/components/Answer.jsx
+++ client/components/Answer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import data from '../../server/public/data'
+import Sound from 'react-sound';
 
 class Answer extends React.Component {
     constructor(props) {
@@ -7,9 +9,12 @@ class Answer extends React.Component {
 
         this.state = {
             buttonClicked:false,
+            soundPlaying:false,
         }
 
         this.onButtonPress = this.onButtonPress.bind(this);
+        this.soundPlayerArea = this.soundPlayerArea.bind(this);
+        this.playSound = this.playSound.bind(this);
 
     }
 
@@ -21,7 +26,7 @@ class Answer extends React.Component {
     }
 
     displayMore(bool){
-      console.log(bool);
+      
       const {category,name}=this.props.match.params
       if (bool) {
         return (
@@ -34,9 +39,49 @@ class Answer extends React.Component {
 
     }
 
+    soundPlayerArea(sound_file) {
+        if (sound_file){
+            return (
+            <React.Fragment>
+            <p> this is where we play {sound_file} v4</p>
+            <button onClick={this.playSound}>Play the sound!</button>
+           {
+               
+               (this.state.soundPlaying) && <Sound url={sound_file} playStatus={Sound.status.PLAYING} onError={((errorCode, description) => console.log(errorCode, description) )}/>
+
+           } 
+            </React.Fragment>
+            )}
+        
+    }
+
+    playSound(){
+
+        this.setState({
+            soundPlaying:!this.state.soundPlaying,
+        })
+
+        const {category,name} =this.props.match.params;
+        const animal = data[category].find( (animal) => animal.name = name);
+        const {sound, sound_file} = animal
+        return <Sound url={sound_file} playStatus={Sound.status.PLAYING} onError={((errorCode, description) => console.log(errorCode, description) )}/>
+    }
+
+    textSound(sound){
+        if (sound){
+            return <p> this animal makes a {sound} sound!</p>
+        }
+    }
+
     render() {
+        const {category,name} =this.props.match.params;
+        const animal = data[category].find( (animal) => animal.name = name);
+        const {sound, sound_file} = animal
+        console.log(sound, ':', sound_file)
         return (
             <div className="Answer">
+                {this.textSound(sound)}
+                {this.soundPlayerArea(sound_file)}
                 <p> What animal is it?? </p>
                 <input type="text"/>
                 <button onClick={this.onButtonPress} >???</button>
--- server/public/data.js
+++ server/public/data.js
@@ -6,7 +6,7 @@ const animals = {
       {name:'Tuatara', silhouette:'http://www.supercoloring.com/sites/default/files/silhouettes/2015/07/tuatara-black-silhouette.svg', img:'https://www.pottonandburton.co.nz/wp-content/uploads/2015/09/tuatara.png', facts:'https://en.wikipedia.org/wiki/Tuatara'}
     ],
     mammals: [
-      {name:'Tiger', silhouette:'https://image.shutterstock.com/image-vector/vector-isolated-silhouette-tiger-260nw-458811232.jpg', img:'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Royal_Bengal_Tiger_at_Kanha_National_Park.jpg/800px-Royal_Bengal_Tiger_at_Kanha_National_Park.jpg', facts:'https://en.wikipedia.org/wiki/Tiger'}
+      {name:'Tiger', silhouette:'https://image.shutterstock.com/image-vector/vector-isolated-silhouette-tiger-260nw-458811232.jpg', img:'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Royal_Bengal_Tiger_at_Kanha_National_Park.jpg/800px-Royal_Bengal_Tiger_at_Kanha_National_Park.jpg', facts:'https://en.wikipedia.org/wiki/Tiger', sound:'roaring', sound_file:'./Tiger7.mp3'}
     ],
     fish:[
       {name:'Piranha', silhouette:'https://www.spreadshirt.com/image-server/v1/mp/designs/13180742,width=178,height=178/piranha-silhouette.png', img:'https://www.sickchirpse.com/wp-content/uploads/2015/03/Piranha.jpg', facts:'https://en.wikipedia.org/wiki/Piranha'}
